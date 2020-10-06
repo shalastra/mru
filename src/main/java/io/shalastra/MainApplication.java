5a198a51e85fe03313f07e53ee2126010b722413
Szymon Halastra <szymon.halastra@gmail.com>
2020-10-06 20:01:34 +0200

    Reorganize imports

--- src/main/java/io/shalastra/MainApplication.java
+++ src/main/java/io/shalastra/MainApplication.java
@@ -1,20 +1,15 @@
 package io.shalastra;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Collections;
 import java.util.stream.Stream;
 
 public class MainApplication {
 
     public static void main(String[] args) {
+        System.out.println(args[0]);
         System.out.println("You have to enter the right path");
 
         Path basePath = Path.of("/home/szymon/Documents/Projects/zio-projects");
