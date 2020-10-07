57b28c76018ab93b7bdee1f783068f8799cbd03b
Szymon Halastra <szymon.halastra@gmail.com>
2020-10-07 21:24:34 +0200

    Allow adding path

--- src/main/java/io/shalastra/GitUpdate.java
+++ src/main/java/io/shalastra/GitUpdate.java
@@ -1,27 +1,31 @@
 package io.shalastra;
 
+import picocli.CommandLine.Model.CommandSpec;
+
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.concurrent.Callable;
 import java.util.stream.Stream;
 
-import static picocli.CommandLine.Command;
+import static picocli.CommandLine.*;
 
 @Command(name = "gup", mixinStandardHelpOptions = true,
         version = "gup 0.0.1",
         description = "Updates all git repositories (current branch only) in the provided path.")
 public class GitUpdate implements Callable<String> {
 
+    @Spec
+    private CommandSpec spec;
+
+    @Parameters(index = "0", description = "The path to root folder containing git repositories.")
+    private Path path;
+
     @Override
     public String call() {
-        String providedPath = ".";
-
-        System.out.print(getClass().getPackage().getImplementationVersion());
-
-        Path basePath = Path.of(providedPath);
-        try (Stream<Path> paths = Files.list(basePath)) {
+        try (Stream<Path> paths = Files.list(path)) {
             paths.filter(Files::isDirectory).forEach(path -> {
                 try {
                     Files.walkFileTree(path, Collections.emptySet(), 1,
@@ -30,6 +34,10 @@ public class GitUpdate implements Callable<String> {
                     e.printStackTrace();
                 }
             });
+        } catch (NoSuchFileException ex) {
+            throw new ParameterException(spec.commandLine(),
+                    String.format("Invalid value '%s' for path: " +
+                            "value is not a path.", path.toString()));
         } catch (IOException e) {
             e.printStackTrace();
         }
